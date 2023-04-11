aa48d5a8c97cd53c4e47d85dcfc6ab5c6df0df76
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-04-11 23:10:47 +0200

    incremental selection & default packages

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua" },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<S-Up>",     -- normal mode
+        node_incremental = "<S-Up>",   -- visual mode
+        node_decremental = "<S-Down>", -- visual mode
+      },
+    },
   },
 }
