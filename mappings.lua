27852cf2e6de53d58d32c3ccc948d363404ae5ff
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-04-19 00:27:11 +0200

    remove already defined mapping

--- mappings.lua
+++ mappings.lua
@@ -1,6 +1,7 @@
 return {
   n = {
-    ["<leader>e"] = { "<cmd>:Neotree source=filesystem toggle=true position=left<cr>", desc = "Toggle Explorer" }
+    ["<leader>1"] = { "<cmd>:Neotree source=filesystem toggle=true position=left<cr>", desc = "Toggle File Explorer" },
+    ["<leader>9"] = { "<cmd>:Neotree source=git_status toggle=true position=left<cr>", desc = "Toggle Git Explorer" },
   },
   t = {
   },
